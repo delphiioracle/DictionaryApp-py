3c1b2a93e8abc9693038d4dd63e3cdb08387fde6
delphiioracle <titiodunewu@gmail.com>
2023-09-30 09:41:37 +0100

    Implemented Menu

---
+++ DictionaryApp.py
@@ -0,0 +1,10 @@
+dictionary = {}
+
+def display_menu():
+    print("\nDictionary App Menu:")
+    print("1. Add a Word")
+    print("2. Delete a Word")
+    print("3. Edit a Word")
+    print("4. View Dictionary")
+    print("5. Search for a Word")
+    print("6. Exit")
